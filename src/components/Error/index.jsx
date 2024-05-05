32feb43752111624418ec2a54de0762e007d3402
Gurvan B <gbourgeron@gmail.com>
2024-05-05 17:58:08 +0200

    404 ERROR file added

---
+++ src/components/Error/index.jsx
@@ -0,0 +1,10 @@
+function Error() {
+    return (
+        <div>
+            <h1>404</h1>
+            <h2>Oups! La page que vous avez demandez n'existe pas.</h2>
+        </div>
+    )
+}
+
+export default Error
--- src/index.js
+++ src/index.js
@@ -3,17 +3,19 @@ import ReactDOM from 'react-dom/client';
 import Home from './pages/Home';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Apropos from './pages/Apropos';
+import Error from './components/Error';
 import Header from './components/Header';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
+      <Header />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/a-propos' element={<Apropos />} />
+        <Route path='*' element={<Error />} />
       </Routes>
-        <Header />
     </Router>
   </React.StrictMode>
 );
