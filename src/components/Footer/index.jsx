5c7eabbc460a3027273ff5c6373564e6f0aa6f20
Gurvan B <gbourgeron@gmail.com>
2024-05-05 18:02:10 +0200

    FOOTER added

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+    return (
+        <div>
+            <h3>Kasa</h3>
+            <p>2020 Kasa. All rights reserved</p>
+        </div>
+    )
+}
+
+export default Footer
--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Apropos from './pages/Apropos';
 import Error from './components/Error';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
       <Header />
+      <Footer />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/a-propos' element={<Apropos />} />
