84265071ba1b7d00ed11151dce9f2a21cb1bfbb4
Gurvan B <gbourgeron@gmail.com>
2024-05-12 08:43:04 +0200

    LAYOUT added

--- src/components/AppRouter/index.jsx
+++ src/components/AppRouter/index.jsx
@@ -1,22 +1,21 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from "../Layout";
 import Home from '../../pages/Home';
 import Apropos from '../../pages/Apropos';
 import Error from '../../components/Error';
-import Header from '../../components/Header';
-import Footer from '../../components/Footer';
 
 function AppRouter() {
     return (
         <Router>
-        <Header />
-        <Footer />
-        <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/a-propos' element={<Apropos />} />
-            <Route path='*' element={<Error />} />
-        </Routes>
-    </Router>
+            <Layout>
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='/a-propos' element={<Apropos />} />
+                    <Route path='*' element={<Error />} />
+                </Routes>
+            </Layout>
+        </Router>
     )
 }
 
