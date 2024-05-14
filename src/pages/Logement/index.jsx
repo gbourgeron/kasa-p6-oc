ffafbca400f38ece2bc6d431ff1e38e0a5dbaf94
Gurvan B <gbourgeron@gmail.com>
2024-05-14 17:41:06 +0200

    LOGEMENT page with datas 1st part

--- src/pages/Logement/index.jsx
+++ src/pages/Logement/index.jsx
@@ -1,21 +1,59 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import Slideshow from "../../components/Slideshow";
+import Collapse from "../../components/Collapse";
 import logements from '../../logements.json';
+import './index.scss';
 
 function LogementPage () {
     const { id } = useParams();
 
     const logement = logements.find(logement => logement.id === id );
 
+    if (!logement) {
+        return <div>Logement introuvable</div>;
+    }
+
     return (
         <div>
+            <Slideshow images={logement.pictures} />
             <div>
-                <h2>{logement.name}</h2>
-                <Slideshow images={logement.pictures} />
+                <div>
+                    <div className="header-logement">
+                        <h2>{logement.title}</h2>
+                        <h3>{logement.location}</h3>
+                    </div>
+                    <div className="header-avatar">
+                        <h4>{logement.host.name}</h4>
+                        <img src={logement.host.picture} alt="Host" />
+                    </div>
+                </div>
+                <div>
+                    <div className="tags">
+                        {logement.tags.map((tag, index) => (
+                            <span key={index}>{tag}</span>
+                        ))}
+                    </div>
+                    <div>
+                        {/* <StarRating rating={logement.rating} /> */}
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <Collapse title="Description" content={logement.description}>
+                        </Collapse>
+                        <Collapse title="Équipements" content={logement.equipments}>
+                            <ul>
+                                {logement.equipments.map((equipment, index) => (
+                                    <li key={index}>{equipment}</li>
+                                ))}
+                            </ul>
+                        </Collapse>
+                    </div>
+                </div>
             </div>
         </div>
-    );
+);
 };
 
 export default LogementPage;
