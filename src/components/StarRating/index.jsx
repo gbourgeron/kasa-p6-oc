47381bfe304637e1b6a530c33bf68ac68f5fa359
Gurvan B <gbourgeron@gmail.com>
2024-05-15 15:38:21 +0200

    STAR RATING part 2 img

--- src/components/StarRating/index.jsx
+++ src/components/StarRating/index.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import './index.scss';
+import starFill from '../../assets/star-fill.svg';
+import starEmpty from '../../assets/star-empty.svg';
 
 function StarRating({ rating }) {
   const stars = [];
   for (let i = 0; i < 5; i++) {
     if (i < rating) {
-      stars.push(<span key={i} className="star filled">&#9733;</span>); // Étoile pleine si i < rating
+      stars.push(<img key={i} src={starFill} alt="Filled star" className="star" />); // Étoile pleine si i < rating
     } else {
-      stars.push(<span key={i} className="star">&#9734;</span>); // Étoile vide sinon
+      stars.push(<img key={i} src={starEmpty} alt="Empty star" className="star" />); // Étoile vide sinon
     }
   }
 
