a693a0881c8e371b2345038d4966e4cf95a58361
Gurvan B <gbourgeron@gmail.com>
2024-05-19 11:45:57 +0200

    RESPONSIVE home

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -2,10 +2,18 @@ import './index.scss';
 
 function Banner({ title, isHome }) {
     const backgroundClass = isHome ? 'background--home' : 'background--about';
+    const titleParts = title.split(', ');
 
     return (
         <section className="banner">
-            {title && <h2>{title}</h2>}
+            {titleParts && (
+                <h2>
+                    <span className='first-part'>{titleParts[0]}, </span>
+                    <br className='mobile-br' />
+                    <span className='second-part'>{titleParts[1]}</span>
+                </h2>
+            )}
+            {/* {title && <h2>{title}</h2>} */}
             <div className={`background ${backgroundClass}`}></div>
         </section>
     )
