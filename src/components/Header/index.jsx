fe5113803170e7e8d4781953b1b8cd119c516c7d
Gurvan B <gbourgeron@gmail.com>
2024-05-19 07:20:15 +0200

    NAVLINK underlined when active

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,16 +1,18 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import logo from '../../assets/LOGO.svg';
 import './index.scss';
 
 function Header() {
+    const location = useLocation();
+    
     return (
         <header>
             <Link to="/" className="logo-link">
                 <img src={logo} alt="Logo Kasa" />
             </Link>
             <nav className="navbar">
-                <Link to="/" className="navbar__link">Accueil</Link>
-                <Link to="/a-propos" className="navbar__link">À propos</Link>
+                <Link to="/" className={`navbar__link ${location.pathname === "/" ? "active" : ""}`}>Accueil</Link>
+                <Link to="/a-propos" className={`navbar__link ${location.pathname === "/a-propos" ? "active" : ""}`}>À Propos</Link>
             </nav>
         </header>
     )
