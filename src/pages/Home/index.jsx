72ebd8a6cf5818f55a2e1067d71d31ea0ad4ff29
Gurvan B <gbourgeron@gmail.com>
2024-05-20 16:59:31 +0200

    RESPONSIVE middle

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -6,14 +6,22 @@ function Home() {
   return (
     <main>
       <Banner title="Chez vous, partout et ailleurs" isHome={true} />
-      <section className="gallery">
+      {/* <section className="gallery">
         <ul>
         {logementsListe.map(({ id, title, cover }) => (
           <Card key={id} id={id} title={title} cover={cover} />
         ))}
         </ul>
-
+      </section> */}
+      {/* <section className="gallery"> */}
+      <section>
+        <ul className="gallery">
+        {logementsListe.map(({ id, title, cover }) => (
+          <Card key={id} id={id} title={title} cover={cover} />
+        ))}
+        </ul>
       </section>
+      {/* </section> */}
     </main>
   );
 }
